2a75f91d230225c5e9c307be90807487919ef824
chukong <408601774@qq.com>
2015-10-05 13:47:52 +0800

    #pragma

--- Film1.0/Film1.0/ViewController.m
+++ Film1.0/Film1.0/ViewController.m
@@ -13,7 +13,7 @@
 @end
 
 @implementation ViewController
-
+#pragma mark - Lifecycle
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
