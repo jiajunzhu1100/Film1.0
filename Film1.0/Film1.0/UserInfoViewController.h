acf8dc66a494a45ff16622a3df08bce6a5fd3ba0
chukong <408601774@qq.com>
2015-10-07 22:55:21 +0800

    update DetailPage

--- Film1.0/Film1.0/UserInfoViewController.h
+++ Film1.0/Film1.0/UserInfoViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "UserLoginInViewController.h"
-#import "DingdanViewController.h";
+#import "DingdanViewController.h"
 
 @class UserInfoViewController;
 @protocol UserInfoViewControllerDelegate <NSObject>
